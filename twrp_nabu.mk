d997c4eb7a01093944b37beb6ebbc892167134ef
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-04-29 15:46:59 -0400

    disabling for now

--- twrp_nabu.mk
+++ twrp_nabu.mk
@@ -20,7 +20,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Virtual A/B Inherits
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+#(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)

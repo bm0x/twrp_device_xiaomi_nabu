c84d39acd7436cc91e1d6274bfb32804b10d49e5
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-05-23 18:06:15 -0400

    inherit for permissive mode

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,6 +42,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := postrecoveryboot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/postrecoveryboot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sensors.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC

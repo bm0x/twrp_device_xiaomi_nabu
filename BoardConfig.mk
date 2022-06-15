ebe52172c6084861bdac2320835b2a36105c5119
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-06-15 18:39:37 -0400

    nabu: Remove virtual framebuffer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,6 @@ BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 \
 		androidboot.console=ttyMSM0 \
 		androidboot.memcg=1 \
 		lpm_levels.sleep_disabled=1 \
-		video=vfb:640x400,bpp=32,memsize=3072000 \
 		msm_rtb.filter=0x237 \
 		service_locator.enable=1 \
 		androidboot.usbcontroller=a600000.dwc3 \
@@ -156,9 +155,9 @@ BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 \
 		cgroup.memory=nokmem,nosocket \
 		reboot=panic_warm \
 		androidboot.init_fatal_reboot_target=recovery \
-        androidboot.selinux=permissive \
-        androidboot.vbmeta.avb_version=1.0 \
-        androidboot.boot_devices=soc/1d84000.ufshc
+        	androidboot.selinux=permissive \
+        	androidboot.vbmeta.avb_version=2.0 \
+        	androidboot.boot_devices=soc/1d84000.ufshc
 
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_KERNEL_IMAGE_NAME := Image
